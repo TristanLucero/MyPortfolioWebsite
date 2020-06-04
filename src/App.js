5a9513bf9c7f8f5f3b9d5a3c069805a6aee594b7
Tristan Lucero <tristanandrewlucero@gmail.com>
2020-06-03 19:25:17 -0500

    simple update to links

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
             <i class="material-icons md-dark">description</i>
             <p className="topbutton-text">Resume</p>
           </a>
-          <a className="buttons-images" href="https://www.google.com">
+          <a className="buttons-images" href="https://github.com/TristanLucero">
             <i class="material-icons md-dark">build_circle</i>
             <p className="topbutton-text">Github</p>
           </a>
@@ -33,14 +33,14 @@ function App() {
       <div className='experience-panel'>
         <p style={{ fontSize: "25px", fontWeight: "bold" }}>Projects:</p>
         <p className='projects-text'>⚫ Portfolio Website: A simple portfolio website, made with React JS and CSS.
-        <img className='project1-img' src="https://i.imgur.com/v0Yy8LF.png"></img>
+        
         <br/>
         <br/> 
             Github  
-            <a href="https://github.com/">
+            <a href="https://github.com/TristanLucero/portfolio-website">
             <img border="0" alt="github project 1" src="https://www.freepngimg.com/thumb/github/62620-github-network-icons-computer-graphics-logo-portable.png" width="30" height="30" />
             </a>
-
+            <img className='project1-img' src="https://i.imgur.com/v0Yy8LF.png"></img>
         </p>
       </div>
       <p className="footer-text">Tristan Lucero's Portfolio Website - Built with React</p>
