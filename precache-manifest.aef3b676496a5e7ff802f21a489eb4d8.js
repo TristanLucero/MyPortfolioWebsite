bb6efaaa1b48056d8447177263cab065013ebefc
Tristan Lucero <tristanandrewlucero@gmail.com>
2020-12-06 12:02:54 -0600

    Updates

---
+++ precache-manifest.aef3b676496a5e7ff802f21a489eb4d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153087a0f8953dc66279a87e149551fc",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "8192a98809a334e718ee",
+    "url": "/my-app/static/css/2.9e5c3921.chunk.css"
+  },
+  {
+    "revision": "4ad4eae9ed37ff50e7db",
+    "url": "/my-app/static/css/main.b1dc3898.chunk.css"
+  },
+  {
+    "revision": "8192a98809a334e718ee",
+    "url": "/my-app/static/js/2.daf65a56.chunk.js"
+  },
+  {
+    "revision": "3a5402f5ddd536a8f5deb8d906cf7b41",
+    "url": "/my-app/static/js/2.daf65a56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ad4eae9ed37ff50e7db",
+    "url": "/my-app/static/js/main.ebce7cc2.chunk.js"
+  },
+  {
+    "revision": "660024b2ea62aa0a0b7b",
+    "url": "/my-app/static/js/runtime-main.5119dbb0.js"
+  }
+]);
